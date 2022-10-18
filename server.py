ac2092493c98c34dbda2dd857b39b826526c1962
Herland0 <maciejbie20@gmail.com>
2022-10-18 22:16:04 +0200

    Finish nicknames

--- server.py
+++ server.py
@@ -3,20 +3,39 @@ from _thread import *
 
 host = "127.0.0.1"
 port = 1234
+clients = []
+nicknames = []
 
 def client_handler(connection):
     while True:
         data = connection.recv(2048)
         message = data.decode('utf-8')
-        if message == 'BYE':
-            break
+        #if message == 'BYE':
+        #     break
+        #reply = f"Server: {message}"
+        #connection.sendall(str.encode(reply))
         reply = f"Server: {message}"
-        connection.sendall(str.encode(reply))
+        if message == "Bye":
+            break
+        for client in clients:
+            if client == connection:
+                continue
+            else:
+                client.send(str.encode(reply))
+            
+
     connection.close()
 
 def accept_connections(ServerSocket):
     Client, address = ServerSocket.accept()
     print(f'Connected to: {address[0]}:{str(address[1])}')
+
+    clients.append(Client)
+    nickname = Client.recv(1024)
+    nicknames.append(nickname.decode('utf-8'))
+    nick_message = f"Nickname set to: {nickname}".encode('utf-8')
+    Client.send(nick_message)
+
     start_new_thread(client_handler, (Client,))
 
 def start_server(host, port):
